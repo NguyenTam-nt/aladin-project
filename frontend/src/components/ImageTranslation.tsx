2bf5e759aeb7088f28df5046f63dd6e26f631d07
nguyentam <tamnt@aladintech.co>
2023-05-11 23:39:33 +0700

    update code

--- frontend/src/components/ImageTranslation.tsx
+++ frontend/src/components/ImageTranslation.tsx
@@ -8,12 +8,20 @@ export const ImageTranslation = ({ link }: { link: string }) => {
     const { offsetLeft, offsetTop } = e.currentTarget;
     const offsetX = pageX - offsetLeft;
     const offsetY = pageY - offsetTop;
+    refImage.current!.style.transition = "all 0.25s ease"
     refImage.current!.style.transformOrigin = `${offsetX}px ${offsetY}px`;
   };
+
+  const handleMouseOut = (e: MouseEvent<HTMLImageElement>) => {
+
+    refImage.current!.style.transition = "all 0.25s cubic-bezier(0.77, 0, 0.175, 1)"
+  };
+
   return (
     <img
       ref={refImage}
       onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseOut}
       className="w-full  object-cover h-full image_hover"
       src={link}
       alt=""
