0371114be511db7eaadc83438b418838235b8213
ngubv <ngubv@aladintech.co>
2023-05-17 11:51:25 +0700

    update code

--- frontend/src/features/libraryImage/index.tsx
+++ frontend/src/features/libraryImage/index.tsx
@@ -2,11 +2,13 @@ import { HeaderTilteLink } from "@components/HeaderTilteLink";
 import React from "react";
 import ImagesList from "./components/ImageList";
 import { Banner } from "@features/abouts/components/Banner";
+import { LinkPageHeader } from "@components/LinkPageHeader";
 
 const LibraryImage = () => {
   return (
     <div>
       <Banner></Banner>
+      <LinkPageHeader />
       <HeaderTilteLink></HeaderTilteLink>
       <ImagesList></ImagesList>
     </div>
