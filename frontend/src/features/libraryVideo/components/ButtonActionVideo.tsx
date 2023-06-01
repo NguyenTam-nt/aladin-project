90c6034f5094d69c70598f424689dae09c40712e
ngubv <ngubv@aladintech.co>
2023-06-01 17:15:27 +0700

    update code

--- frontend/src/features/libraryVideo/components/ButtonActionVideo.tsx
+++ frontend/src/features/libraryVideo/components/ButtonActionVideo.tsx
@@ -14,11 +14,11 @@ export const ButtonActionVideo = ({onPlayNow}:Props) => {
         text="button.watch_now"
         className="!w-[150px] flex justify-center  bg-secondary !h-[40px] !py-[2px] !text-_16"
       />
-      <Button
+      {/* <Button
         className="!w-[150px] flex justify-center  bg-transparent border-secondary border-[1px] !h-[40px] !py-[2px] !text-_16 ml-[24px]"
         text="button.share"
         color="empty"
-      />
+      /> */}
     </>
   );
 };
