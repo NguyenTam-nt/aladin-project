fec2333e8abb40226ea012d3667c0817eeb04d65
CuongNM <cuongnm@aladintech.co>
2023-06-08 18:07:43 +0700

    merge

--- frontend/src/constants/routerPublic.tsx
+++ frontend/src/constants/routerPublic.tsx
@@ -14,7 +14,14 @@ const MenuPage = lazy(() =>
   import("@features/menu").then((module) => ({ default: module.Menu }))
 );
 
+const TableReserVation = lazy(
+  () => import("@features/news/user/TableReserVation")
+);
+const Recruitment = lazy(() => import("@features/recruitment/index"));
+const News = lazy(() => import("@features/news/user/index"));
+const NewDetail = lazy(() => import("@features/news/user/NewDetail"));
 const ContactPage = lazy(() => import("@features/contact"));
+
 const AboutUsPage = lazy(() => import("@features/about-us"));
 
 const DemoElement = () => {
@@ -50,55 +57,53 @@ export const paths = {
   },
 };
 
+export const routersPublic: IRouter[] = [
+  {
+    path: paths.home.prefix,
+    element: HomePage,
+    name: "",
+    isHiden: true,
+  },
+  {
+    path: paths.memu.prefix,
+    element: MenuPage,
+    name: "navigation.header.memu",
+  },
+  {
+    path: paths.sale.prefix,
+    element: News,
+    name: "navigation.header.sale",
+    isHiden: true,
+  },
+  {
+    path: paths.news.prefix,
+    element: News,
+    name: "navigation.header.news",
+  },
+  {
+    path: paths.NewDetail.prefix,
+    element: NewDetail,
+    name: "navigation.header.newDetail",
+  },
+  {
+    path: paths.order.prefix,
+    element: TableReserVation,
+    name: "navigation.header.order",
+  },
 
-export const routersPublic:IRouter[] = [
-    {
-        path: paths.home.prefix,
-        element: HomePage,
-        name: "",
-        isHiden: true,
-    },
-    {
-        path: paths.memu.prefix,
-        element: MenuPage,
-        name: "navigation.header.memu",
-    },
-    {
-        path: paths.sale.prefix,
-        element: News,
-        name: "navigation.header.sale",
-        isHiden: true
-      },
-    {
-        path: paths.news.prefix,
-        element: News,
-        name: "navigation.header.news",
-    },
-    {
-      path: paths.NewDetail.prefix,
-      element: NewDetail,
-      name: "navigation.header.newDetail",
-    },
-   
-    {
-        path: paths.order.prefix,
-        element: TableReserVation,
-        name: "navigation.header.order",
-    },
-   
-    {
-        path: paths.about.prefix,
-        element: AboutUsPage,
-        name: "navigation.header.about",
-    },
-    {
-        path: paths.ecruitment.prefix,
-        element: Recruitment,
-        name: "navigation.header.ecruitment",
-    },
-    {
-        path: paths.contact.prefix,
-        element: ContactPage,
-        name: "navigation.header.contact",
-    }
-]
+  {
+    path: paths.about.prefix,
+    element: AboutUsPage,
+    name: "navigation.header.about",
+  },
+  {
+    path: paths.ecruitment.prefix,
+    element: Recruitment,
+    name: "navigation.header.ecruitment",
+  },
+  {
+    path: paths.contact.prefix,
+    element: ContactPage,
+    name: "navigation.header.contact",
+  },
+];
