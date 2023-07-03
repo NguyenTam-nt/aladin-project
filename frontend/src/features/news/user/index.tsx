89f556d5333e9bf5f7001dfb270f4d6ac4f8dd59
buikhac tao <khactao99@gmail.com>
2023-07-03 14:02:47 +0700

    change size in indexNew

--- frontend/src/features/news/user/index.tsx
+++ frontend/src/features/news/user/index.tsx
@@ -19,13 +19,13 @@ const PromotionPage = () => {
         params
       );
       setListNews(list);
-      setTotalPages(Math.ceil(totalElementPage / 8));
+      setTotalPages(Math.ceil(totalElementPage / 12));
     } catch (error) {
       console.log("Không thể lấy dánh sách tin tức");
     }
   };
   useEffect(() => {
-    getListNew({ page: currentPage - 1, size: 8, sort: `id,desc` });
+    getListNew({ page: currentPage - 1, size: 12, sort: `id,desc` });
   }, [currentPage]);
   return (
     <div className="">
