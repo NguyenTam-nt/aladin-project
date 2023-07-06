fda268d98bde2bf57b67a0d70fdf4aec094c6389
nguyentam <tamnt@aladintech.co>
2023-07-06 14:41:55 +0700

    update code

--- frontend/src/components/MenuSideRight.tsx
+++ frontend/src/components/MenuSideRight.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useMemo, useRef } from "react";
 import { MenusRight } from "./MenusRight";
 import { ContactSilde } from "./contactSilde";
-import { prefixRootRoute, windownSizeHeight, windownSizeWidth, withResponsive } from "@constants/index";
+import {
+  prefixRootRoute,
+  windownSizeHeight,
+} from "@constants/index";
 import clsx from "clsx";
 import { useLocation } from "react-router-dom";
 import { useScreenOrientation } from "@hooks/useScreenOrientation";
@@ -13,34 +16,34 @@ export const MenuSideRight = () => {
     return pathname.includes(prefixRootRoute.admin);
   }, [pathname]);
 
-  useEffect(() => {
-    window.addEventListener(
-      "scroll",
-      () => {
-        if (refScroll.current) {
-          if (window.location.pathname === prefixRootRoute.public) {
-            if (document.documentElement.scrollTop >= windownSizeHeight) {
-              refScroll.current!.style.opacity = "1";
-            } else {
-              refScroll.current!.style.opacity = "0";
-            }
-          } else {
-            refScroll.current!.style.opacity = "1";
-          }
+  const handleScroll = () => {
+    if (refScroll.current) {
+      if (window.location.pathname === prefixRootRoute.public) {
+        if (document.documentElement.scrollTop >= windownSizeHeight) {
+          refScroll.current!.style.opacity = "1";
+        } else {
+          refScroll.current!.style.opacity = "0";
         }
-      },
-      { passive: true }
-    );
+      } else {
+        refScroll.current!.style.opacity = "1";
+      }
+    }
+  };
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
     if (pathname !== prefixRootRoute.public) {
       refScroll.current!.style.opacity = "1";
     } else if (document.documentElement.scrollTop < windownSizeHeight) {
       refScroll.current!.style.opacity = "0";
     }
-
-    return () => {
-      window.removeEventListener("scroll", () => {});
-    };
   }, [pathname]);
 
   const { isFullScreen } = useScreenOrientation();
@@ -55,7 +58,7 @@ export const MenuSideRight = () => {
           "[&>button]:rounded-[8px_0_8px_0] fixed bottom-[calc(7%_+_56px)] 3xl:bottom-[calc(10%_+_78px)]  z-[11]  group-contact right-0   2xl:[&>button]:rounded-[16px_0_16px_0] [&>button]:w-[40px] gap-y-[16px] 2xl:gap-y-[24px] flex flex-col justify-end [&>button]:h-[40px] 2xl:[&>button]:w-[54px] 2xl:[&>button]:h-[54px]",
           {
             "opacity-0 z-[-2]": isAdmin,
-            "!bottom-[16px] gap-y-[12px]": isFullScreen
+            "!bottom-[16px] gap-y-[12px]": isFullScreen,
           }
         )}
       >
