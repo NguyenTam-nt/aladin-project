a63707ab18d73e8367838fad8dc2275417d0ac13
nguyentam <tamnt@aladintech.co>
2024-04-18 13:37:11 +0700

    update code

--- frontend/src/features/menu-detail/components/GroupStar.tsx
+++ frontend/src/features/menu-detail/components/GroupStar.tsx
@@ -1,44 +1,48 @@
-import { commentService } from "@services/comment";
-import type { ICommentDetail } from "@typeRules/comment";
-import React, { memo, useCallback, useEffect, useMemo, useState } from "react";
-import { useParams } from "react-router-dom";
-import { MenuDetailComment } from "./MenuDetailComment";
-import { MenuDetailStar } from "./MenuDetailStar";
-import { Button } from "@components/Button";
-import { ICArowDown } from "@assets/icons/ICArowDown";
-import { Colors } from "@constants/color";
-import { Loading } from "@features/dashboard/components/Loading";
+import { commentService } from "@services/comment"
+import type { ICommentDetail } from "@typeRules/comment"
+import React, { memo, useCallback, useEffect, useMemo, useState } from "react"
+import { useParams } from "react-router-dom"
+import { MenuDetailComment } from "./MenuDetailComment"
+import { MenuDetailStar } from "./MenuDetailStar"
+import { Button } from "@components/Button"
+import { ICArowDown } from "@assets/icons/ICArowDown"
+import { Colors } from "@constants/color"
+import { Loading } from "@features/dashboard/components/Loading"
+import { getSlugId } from "@commons/common"
 
 export const GroupStar = memo(() => {
-  const [comment, setComment] = useState<ICommentDetail>();
-  const [currentPage, setCurrentPage] = useState<number>(0);
-  const [loading, setLoading] = useState<boolean>(false);
-  const params = useParams();
+  const [comment, setComment] = useState<ICommentDetail>()
+  const [currentPage, setCurrentPage] = useState<number>(0)
+  const [loading, setLoading] = useState<boolean>(false)
+  const params = useParams()
   useEffect(() => {
     if (params?.id) {
-      setLoading(true);
+      setLoading(true)
       commentService
-        .getById({ page: 0, size: 4, sort: "id,desc" }, Number(params?.id))
+        .getById(
+          { page: 0, size: 4, sort: "id,desc" },
+          Number(getSlugId(params?.id))
+        )
         .then((data) => {
-          setComment(data);
+          setComment(data)
         })
         .finally(() => {
-          setLoading(false);
-        });
+          setLoading(false)
+        })
     }
-  }, [params?.id]);
+  }, [params?.id])
 
   const totalPage = useMemo(() => {
-    return Math.ceil(Number(comment?.listComment?.totalElementPage ?? 0) / 4);
-  }, [comment?.listComment]);
+    return Math.ceil(Number(comment?.listComment?.totalElementPage ?? 0) / 4)
+  }, [comment?.listComment])
 
   const handleNextPage = useCallback(() => {
     if (currentPage < totalPage - 1) {
-      setLoading(true);
+      setLoading(true)
       commentService
         .getById(
           { page: currentPage + 1, size: 4, sort: "id,desc" },
-          Number(params?.id)
+          getSlugId(params?.id ?? "")
         )
         .then((data) => {
           if (data?.listComment && comment) {
@@ -48,19 +52,22 @@ export const GroupStar = memo(() => {
                 ...data.listComment,
                 list: [...comment?.listComment.list, ...data.listComment.list],
               },
-            });
+            })
           }
-          setCurrentPage((p) => p + 1);
+          setCurrentPage((p) => p + 1)
         })
         .finally(() => {
-          setLoading(false);
-        });
+          setLoading(false)
+        })
     }
-  }, [totalPage, currentPage, comment]);
+  }, [totalPage, currentPage, comment])
 
   return comment ? (
     <>
-      <MenuDetailStar idParent={Number(params?.id)} data={comment} />
+      <MenuDetailStar
+        idParent={Number(getSlugId(params?.id ?? ""))}
+        data={comment}
+      />
       <MenuDetailComment commnets={comment?.listComment?.list || []} />
       {loading ? <Loading /> : null}
       {totalPage > 1 && currentPage < totalPage - 1 ? (
@@ -78,5 +85,5 @@ export const GroupStar = memo(() => {
         />
       ) : null}
     </>
-  ) : null;
-});
+  ) : null
+})
